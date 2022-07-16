5bd9c501c12347f12a797ecfa0b8fae4904229b7
김규년 <42989067+ggn724@users.noreply.github.com>
2022-07-16 21:35:53 +0900

    apms_FE_1st

---
+++ apms-FE/src/component/basic/Function.jsx
@@ -0,0 +1,14 @@
+/*
+import React from "react";
+
+const UserComponent = (props) => {
+    return (
+        <div className="container">
+
+        </div>
+
+    );
+}
+
+export default UserComponent;
+*/
