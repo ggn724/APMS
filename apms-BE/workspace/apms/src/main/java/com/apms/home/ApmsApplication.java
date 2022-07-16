9dc05f2fd94a74c3b0d3093827ff7c9cd067bb26
김규년 <42989067+ggn724@users.noreply.github.com>
2022-07-16 21:11:27 +0900

    ATMS_BE_1st

---
+++ apms-BE/workspace/apms/src/main/java/com/apms/home/ApmsApplication.java
@@ -0,0 +1,13 @@
+package com.apms.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmsApplication.class, args);
+	}
+
+}
---
+++ apms-BE/workspace/apms/src/test/java/com/apms/home/ApmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apms.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
